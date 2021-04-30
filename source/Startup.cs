2cf9d324254b4a3f044baa4c58287cbc1c40aa00
Vladimir Matos <vladimir.matos@outlook.com>
2021-04-29 22:34:18 -0300

    build (initial): initial commit
    add initial version of project

---
+++ source/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Health_Checks_API
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+
+			services.AddControllers();
+			services.AddSwaggerGen(swagger =>
+			{
+				swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Health_Checks_API", Version = "v1" });
+			});
+		}
+
+		public void Configure(IApplicationBuilder application, IWebHostEnvironment environment)
+		{
+			if (environment.IsDevelopment())
+			{
+				application.UseDeveloperExceptionPage();
+				application.UseSwagger();
+				application.UseSwaggerUI(swagger => swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "Health-Checks-API v1"));
+			}
+			
+			application.UseHttpsRedirection();
+			application.UseRouting();
+			application.UseAuthorization();
+			application.UseEndpoints(endpoints => { endpoints.MapControllers();});
+		}
+	}
+}
